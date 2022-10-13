474df6a87d088c95160eed20610e9d76d834aff7
12179 <12179@DESKTOP-C4B4UTT.dhcp.wustl.edu>
2022-10-13 12:20:52 -0500

    HEre

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,6 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,19 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String Shape = in.next();
+		int Red = in.nextInt();
+		int Green = in.nextInt();
+		int Blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		Color pen = new Color (Red, Green, Blue);
+		StdDraw.setPenColor(pen);
+		StdDraw.setPenRadius(.05);
+			StdDraw.rectangle(x,y,halfWidth,halfHeight);
+	
 	}
 }
