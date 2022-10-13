7b3ef94eec87f1fcc20c94e81544d96d0fd409cc
12179 <12179@DESKTOP-C4B4UTT.dhcp.wustl.edu>
2022-10-13 12:41:09 -0500

    WOOOOOOOOOOOOOOOOOOOOOOOO

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,7 +6,11 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
-		
+		StdDraw.clear();
+		StdDraw.setCanvasSize(1000,1000);
+		StdDraw.setPenColor(255, 0, 0);
+		StdDraw.filledCircle(.5, .5, .2);
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.filledCircle(.5, .5, .05);
 	}
 }
